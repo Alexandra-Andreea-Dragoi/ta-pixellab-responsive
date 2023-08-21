cd028e5a1653341a9b4a5e2066032f3534a01437
Alexandra Andreea Dragoi <69802787+Alexandra-Andreea-Dragoi@users.noreply.github.com>
2023-08-21 12:20:38 +0300

    third-save-configure-sass-bootstrap-font-awesome

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <header className="header">Header</header>
+      <header className="header">
+        <i className="fa-solid fa-arrow-right"></i>
+      </header>
 
       <main className="content">Body</main>
 
