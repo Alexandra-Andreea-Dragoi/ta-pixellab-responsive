34baad035e95587c9841c23e7b8cf80fbf51645a
Alexandra Andreea Dragoi <69802787+Alexandra-Andreea-Dragoi@users.noreply.github.com>
2023-08-21 11:26:44 +0300

    second-save

---
+++ src/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Thread Affair',
+  description: 'A high quality suit brand',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <header className="header">Header</header>
+
+      <main className="content">Body</main>
+
+      <footer className="footer">Footer</footer>
+    </>
+  );
+}
