70715f30ca0592158dec487b09707fc12e0fb1eb
Alexandra Andreea Dragoi <69802787+Alexandra-Andreea-Dragoi@users.noreply.github.com>
2023-08-31 14:47:48 +0300

    7th

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,3 +1,194 @@
 export default function Footer() {
-  return <>footer</>;
+  return (
+    <>
+      <div className="footer-primary">
+        <div className="footer-primary-inner container">
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <a href="/" title="">
+                  information
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="contact us">
+                  contact us
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="track my order">
+                  track my order
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="shipping">
+                  shipping
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="free returns*">
+                  free returns*
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="my account">
+                  my account
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="servicies">
+                  servicies
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="ship to store">
+                  ship to store
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="gift card">
+                  gift card
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="online only">
+                  online only
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="tips & tricks">
+                  tips & tricks
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="home">
+                  home
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="loyality programs">
+                  loyality programs
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="sign up to the simons">
+                  sign up to the simons
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="my account">
+                  my account
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="conditions">
+                  conditions
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="faq">
+                  faq
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="careers">
+                  careers
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="about us">
+                  about us
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="our story">
+                  our story
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="art & culture">
+                  art & culture
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="our private labels">
+                  our private labels
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="stores">
+                  stores
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <section className="footer-contact">
+            <h1>
+              <span>Thread Affair</span> Milano
+            </h1>
+            <p>
+              Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66
+              office@thread-affair.com
+            </p>
+          </section>
+
+          <section className="footer-connect">
+            <ul>
+              <li>
+                <a href="" title="">
+                  <i className="fa-solid"></i>
+                  support
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  <i className="fa-solid"></i>
+                  chat
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  <i className="fa-solid"></i>
+                  email
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </div>
+
+      <div className="footer-secondary container">do your homework</div>
+    </>
+  );
 }
